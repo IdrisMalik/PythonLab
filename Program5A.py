7c36771aaed1e6b7ff99be9f9df43e91af48e2b1
IdrisMalik <89416208+IdrisMalik@users.noreply.github.com>
2023-09-08 11:52:19 +0530

    Update and rename Program5.py to Program5A.py

--- Program5A.py
+++ Program5A.py
@@ -28,16 +28,3 @@ if chkphonenumber(ph_num):
  print("Valid phone number")
 else:
  print("Invalid phone number")
-
-#Search Phone Number and e-mail
-import re
-phone_regex = re.compile(r'\+\d{12}')
-email_regex = re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
-with open('example.txt', 'r') as f:
- for line in f:
- matches = phone_regex.findall(line)
- for match in matches:
- print(match)
- matches = email_regex.findall(line)
- for match in matches:
- print(match) 

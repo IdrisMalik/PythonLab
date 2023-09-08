f584919b89dd9380c6a04aa207cb32577ff30e01
IdrisMalik <89416208+IdrisMalik@users.noreply.github.com>
2023-09-08 11:49:53 +0530

    Update and rename Program3.py to Program3A.py

--- Program3A.py
+++ Program3A.py
@@ -11,19 +11,3 @@ for ch in sentence:
  elif 'a' <= ch <= 'z':
  loCnt += 1
 print("This sentence has", digCnt, "digits", upCnt, "upper case letters", loCnt,"lower case letters")
-
-#String Similarity
-str1 = input("Enter String 1 \n")
-str2 = input("Enter String 2 \n")
-if len(str2) < len(str1):
- short = len(str2)
- long = len(str1)
-else:
- short = len(str1)
- long = len(str2)
-matchCnt = 0
-for i in range(short):
- if str1[i] == str2[i]:
- matchCnt += 1
-print("Similarity between two said strings:")
-print(matchCnt/long)
